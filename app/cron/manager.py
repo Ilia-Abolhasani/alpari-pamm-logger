7afa9620d7d6b6d681a3745fab5ec1ee47fd2c10
Ilia Abolhasani <abolhasani.eliya@gmail.com>
2023-10-19 11:16:44 +0330

    refresh login added to cron job.

--- app/cron/manager.py
+++ app/cron/manager.py
@@ -5,11 +5,17 @@ import app.cron.job_channel_add_message as job_channel_add_message
 
 def start_jobs(alpari, bot_api, latest_message_file_handler):
     scheduler = BackgroundScheduler(
-        {'apscheduler.job_defaults.max_instances': 1})
+        {'apscheduler.job_defaults.max_instances': 2})
     # job add message to channel
     scheduler.add_job(
         lambda: job_channel_add_message.start(
             alpari, bot_api, latest_message_file_handler),
         trigger=CronTrigger.from_crontab('*/5 * * * *')
+    )
+
+    # job refresh token
+    scheduler.add_job(
+        lambda: alpari.login(),
+        trigger=CronTrigger.from_crontab('1 */6 * * *')
     )
     scheduler.start()
